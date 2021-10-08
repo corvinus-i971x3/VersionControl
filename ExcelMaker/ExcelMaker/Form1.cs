5c9b3c2a7564b833f8410ef864fc2bb2bd55ef37
Richárd Bárdos <richard.bardos@stud.uni-corvinus.hu>
2021-10-08 10:29:21 +0200

    add excel namespace

--- ExcelMaker/ExcelMaker/Form1.cs
+++ ExcelMaker/ExcelMaker/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelMaker
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> lakasok;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+
+            //Missing.Value
+        }
+
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
         }
     }
 }
