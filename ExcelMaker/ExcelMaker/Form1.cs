14a311ed33a4aaa4f5fa8eda5e1905321a444a91
Richárd Bárdos <richard.bardos@stud.uni-corvinus.hu>
2021-10-08 12:37:48 +0200

    Excel kreáció és feltöltés

--- ExcelMaker/ExcelMaker/Form1.cs
+++ ExcelMaker/ExcelMaker/Form1.cs
@@ -16,12 +16,17 @@ namespace ExcelMaker
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+
+            CreateExcel();
 
             //Missing.Value
         }
@@ -30,5 +35,106 @@ namespace ExcelMaker
         {
             lakasok = context.Flats.ToList();
         }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet; //épp aktív
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+            finally //mindenképp lefut - ha hiba van ha nem
+            {
+
+            }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"};
+
+            //xlSheet.Cells[1, 1] = "Hello"; //legeslő cellaba irás
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[lakasok.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in lakasok)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                string v1 = GetCell(counter + 2, 6);
+                string v2 = GetCell(counter + 2, 7);
+                string vv = "=" + v2 + "/" + v1;
+                values[counter, 8] = vv;
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            //FORMÁZÁS
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
     }
 }
