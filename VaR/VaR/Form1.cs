82a0fe9399e14c93bdf3bba046ab272b49fbac37
Richárd Bárdos <richard.bardos@stud.uni-corvinus.hu>
2021-10-22 10:41:14 +0200

    StreamWriteer + SaveFileDialog

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,8 @@ namespace VaR
 
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
+        List<decimal> nyereségekRendezve = new List<decimal>();
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +28,25 @@ namespace VaR
             dataGridView1.DataSource = Ticks;
 
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
@@ -39,6 +61,38 @@ namespace VaR
 
             dataGridView2.DataSource = Portfolio;
 
+        }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ShowDialog();
+
+            StreamWriter sw = new StreamWriter(sfd.FileName);
+            sw.WriteLine("Időszak - Nyereség");
+
+            int i = 1;
+            foreach (var item in nyereségekRendezve)
+            {
+                sw.WriteLine(i + " - " + item);
+                i++;
+            }
+
+            sw.Close();
         }
     }
 }
